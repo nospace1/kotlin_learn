38ef1538c61cb5e7071060ecf520db3cb8dd075f
woojuan <dnwn010194@gmail.com>
2021-01-03 18:22:00 +0900

    Hong joyce 2강 Singleton Pattern

---
+++ app/src/main/java/com/example/practice/sample2.kt
@@ -0,0 +1,22 @@
+package com.example.practice
+
+// Singleton Pattern : 한번만 객체를 생성 ex) Car
+object CarFactory {
+    val cars = mutableListOf<Car>()
+    fun makeCar(horsePower: Int) : Car {
+        val car = Car(horsePower)
+        cars.add(car)
+        return car
+    }
+}
+
+data class Car(val horsePower : Int)
+
+fun main(){
+    val car = CarFactory.makeCar(10)
+    val car2 = CarFactory.makeCar(200)
+
+    println(car)
+    println(car2)
+    println(CarFactory.cars.size.toString())
+}
